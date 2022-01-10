503e3379805da735604e8e26d58c65fddbec34c9
TAKANO Mitsuhiro <takano32@gmail.com>
2022-01-10 13:38:00 +0900

    Add Login Form

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,6 +4,31 @@
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">ログイン</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="name"
+              label="ユーザ名"
+              prepend-icon="mdi-account-circle"/>
+            <v-text-field
+              v-model="password"
+              label="パスワード"
+              prepend-icon="mdi-lock"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"/>
+            <v-card-actions>
+              <v-btn
+                class="info"
+                @click="submit">ログイン</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
       <v-container>
         <Nuxt />
       </v-container>
@@ -16,6 +41,14 @@ export default {
   data() {
     return {
       title: 'Vuetify.js',
+      showPassword : false,
+      name:'',
+      password:'',
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.name,this.password)
     }
   },
 }
