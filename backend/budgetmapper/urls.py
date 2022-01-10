e7a891594c13805adc03a2344003e4a43c34f657
osoken <osoken.devel@outlook.jp>
2022-01-10 18:37:25 +0900

    feature(backend): init with django and implement prototypes of models, views and urls with DRF.

---
+++ backend/budgetmapper/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework_nested import routers
+
+from . import views
+
+router = routers.DefaultRouter(trailing_slash='/?')
+router.register(r"governments", views.GovernmentViewSet)
+router.register(r"classifiaction-systems", views.ClassificationSystemViewSet)
+router.register(r"budgets", views.BudgetViewSet)
+
+classification_system_router = routers.NestedDefaultRouter(
+    router, r"classifiaction-systems", lookup=r"classification_system"
+)
+classification_system_router.register(
+    r"classifications", views.ClassificationViewSet, basename="classification-system-classification"
+)
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+    path('api/v1/', include(classification_system_router.urls)),
+]
---
+++ backend/budgetmapper/views.py
@@ -0,0 +1,46 @@
+import django_filters.rest_framework as drf_filters
+from django_filters import rest_framework as filters
+from rest_framework import viewsets
+from rest_framework.pagination import CursorPagination
+
+from . import models, serializers
+
+
+class CreatedAtPagination(CursorPagination):
+    page_size = 10
+    ordering = '-created_at'
+
+
+class UpdatedAtPagination(CursorPagination):
+    page_size = 10
+    ordering = '-updated_at'
+
+
+class GovernmentViewSet(viewsets.ModelViewSet):
+    queryset = models.Government.objects.all()
+    serializer_class = serializers.GovernmentSerializer
+    pagination_class = CreatedAtPagination
+
+
+class ClassificationSystemViewSet(viewsets.ModelViewSet):
+    queryset = models.ClassificationSystem.objects.all()
+    serializer_class = serializers.ClassificationSystemSerializer
+    pagination_class = CreatedAtPagination
+
+
+class ClassificationViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        return models.Classification.objects.filter(classification_system=self.kwargs['classification_system_pk'])
+
+    serializer_class = serializers.ClassificationSerializer
+    pagination_class = CreatedAtPagination
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = models.Budget.objects.all()
+    pagination_class = CreatedAtPagination
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return serializers.BudgetDetailSerializer
+        return serializers.BudgetSerializer
