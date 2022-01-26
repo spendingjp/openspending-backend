8de92c7e556e36d59fe1e380919e9f55e3d9e6ae
osoken <osoken.devel@outlook.jp>
2022-01-26 22:48:49 +0900

    feature(backend): add loading fixture for xlsx budget template file

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,4 +4,5 @@ python /app/backend/manage.py makemigrations
 python /app/backend/manage.py makemigrations budgetmapper
 python /app/backend/manage.py migrate
 python /app/backend/manage.py collectstatic
+python /app/backend/manage.py loaddata /app/backend/budgetmapper/fixtures/budget_xlsx_template.json
 python /app/backend/manage.py runserver 0.0.0.0:8000
