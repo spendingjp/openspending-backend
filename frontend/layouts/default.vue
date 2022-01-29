ddb189d3c183261dc51505e1318dc5e963c8f989
Kazuki Imamura <kaizumaki@gmail.com>
2022-01-29 20:52:33 +0900

    テンプレートダウンロードのリンクボタンを追加

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,6 +3,13 @@
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-btn
+        class="mr-4"
+        outlined
+        :href="`${$config.apiUrl}/transfer/xlsx_template`"
+      >
+        テンプレートをダウンロード
+      </v-btn>
       <v-btn outlined @click="handleLogout">ログアウト</v-btn>
     </v-app-bar>
     <v-main>
