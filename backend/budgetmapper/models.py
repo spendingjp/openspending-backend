0dfde1b75d18f8bb356e1e13d3c88a327bd49d42
yuuki.ootaki <ootaki.yuuki@gmail.com>
2022-02-23 21:03:23 +0900

    feature(backend): add defaultBudget model

--- backend/budgetmapper/models.py
+++ backend/budgetmapper/models.py
@@ -172,11 +172,11 @@ class IconImage(models.Model):
                 b'w3.org/1999/xlink" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-backgr'
                 b'ound="new 0 0 100 100" xml:space="preserve"><circle fill="#B2B2B2" cx="50" cy="50" r="50"/><g><path'
                 b' d="M46.745,79.538c-3.866-0.402-8.458-1.449-12.324-3.705l2.255-6.444c2.819,2.095,6.686,3.786,10.391'
-                b',4.35l1.208-22.876c-5.961-5.075-12.244-10.391-12.244-18.769c0-8.539,6.283-13.21,13.936-13.774l0.483'
-                b'-8.78h5.316l-0.483,9.021c2.578,0.322,5.559,1.128,8.861,2.497l-1.853,5.639c-2.015-1.047-4.753-1.933-'
-                b'7.331-2.336l-1.208,21.588c6.122,5.155,12.646,10.713,12.646,19.655c0,8.457-6.041,13.29-14.419,14.016'
-                b'l-0.563,10.149h-5.155L46.745,79.538z M48.759,41.599l0.886-17.238c-3.544,0.645-6.364,2.9-6.364,7.169'
-                b'C43.281,35.477,45.618,38.619,48.759,41.599z M53.27,55.132l-0.967,18.606c4.189-0.805,6.848-3.705,6.8'
+                b",4.35l1.208-22.876c-5.961-5.075-12.244-10.391-12.244-18.769c0-8.539,6.283-13.21,13.936-13.774l0.483"
+                b"-8.78h5.316l-0.483,9.021c2.578,0.322,5.559,1.128,8.861,2.497l-1.853,5.639c-2.015-1.047-4.753-1.933-"
+                b"7.331-2.336l-1.208,21.588c6.122,5.155,12.646,10.713,12.646,19.655c0,8.457-6.041,13.29-14.419,14.016"
+                b"l-0.563,10.149h-5.155L46.745,79.538z M48.759,41.599l0.886-17.238c-3.544,0.645-6.364,2.9-6.364,7.169"
+                b"C43.281,35.477,45.618,38.619,48.759,41.599z M53.27,55.132l-0.967,18.606c4.189-0.805,6.848-3.705,6.8"
                 b'48-7.894S56.653,58.354,53.27,55.132z"/></g></svg>',
             },
         )[0]
@@ -450,6 +450,20 @@ class WdmmgTreeCache(models.Model):
         if cache.updated_at > budget.updated_at:
             return json.load(BlobReader(cache.blob))
         return None
+
+
+class DefaultBudget(models.Model):
+    id = PkField()
+    government = models.OneToOneField(Government, on_delete=models.CASCADE, db_index=True, null=False, unique=True)
+    budget = models.OneToOneField(BudgetBase, on_delete=models.CASCADE, null=False, db_index=False)
+    created_at = CurrentDateTimeField()
+    updated_at = AutoUpdateCurrentDateTimeField()
+
+    def save(self, *args, **kwargs):
+        if self.budget.government.id != self.government.id:
+            raise ValidationError("A budget must blongs to specified government.")
+
+        super(DefaultBudget, self).save(*args, **kwargs)
 
 
 @receiver(post_save, sender=AtomicBudgetItem)
